7cca20672761704b7d35e3e426d8b76fae3ab319
= <=>
2022-03-01 15:32:39 -0300

    Criei um codigo que persiste categorias se elas nao existirem ainda

--- src/main/java/br/com/comercio/ComercioApplication.java
+++ src/main/java/br/com/comercio/ComercioApplication.java
@@ -1,5 +1,9 @@
 package br.com.comercio;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,11 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mercadopago.MercadoPago;
 
+import br.com.comercio.model.Categoria;
+import br.com.comercio.repository.CategoriaRepository;
+
 @SpringBootApplication
 public class ComercioApplication implements CommandLineRunner {
 
 	@Value("${mercadopago_access_token}")
 	private String mercadoPagoAccessToken;
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComercioApplication.class, args);
@@ -26,6 +35,29 @@ public class ComercioApplication implements CommandLineRunner {
 			MercadoPago.SDK.setAccessToken(mercadoPagoAccessTokenHeroku);
 		} else {
 			MercadoPago.SDK.setAccessToken(mercadoPagoAccessToken);
+		}
+		verificaSePrecisaPersistirCategorias();
+	}
+
+	private void verificaSePrecisaPersistirCategorias() {
+		List<String> nomeCategorias = new ArrayList<String>();
+		nomeCategorias.add("eletrônicos");
+		nomeCategorias.add("eletrodomésticos");
+		nomeCategorias.add("celulares");
+		nomeCategorias.add("diversos");
+		for (String categoria : nomeCategorias) {
+			try {
+				Categoria categoriaBuscada = categoriaRepository.findByNome(categoria);
+				// Se eu não encontrei essa categoria no bd
+				// significa que eu preciso persisti-la
+				if (categoriaBuscada == null) {
+					Categoria categoriaCad = new Categoria();
+					categoriaCad.setNome(categoria);
+					categoriaRepository.save(categoriaCad);
+				}
+			} catch (Exception e) {
+
+			}
 		}
 	}
 
